6b35a4d761c5a746db8dad12ac923b2e4dcd13ed
Dilan Arjuna <arjunadilan@outlook.com>
2018-05-14 12:08:46 +0800

    first commit

---
+++ learn.rb
@@ -0,0 +1,16 @@
+package 'apache' do
+  package_name 'httpd'
+end
+service 'httpd' do
+  action [:start, :enable ]
+end
+
+file '/var/www/index.html' do
+  content 'Hello World'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+
